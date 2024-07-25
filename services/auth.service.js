0fa0139058cbbb13165e3f8456e676b4d52fca49
milindkusahu <milindsahu2000@gmail.com>
2024-07-26 02:02:04 +0530

    Hashing Password

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,9 +1,21 @@
+const bcrypt = require("bcrypt");
 const UserService = require("../services/user.service");
 const UserServiceInstance = new UserService();
 
 class AuthService {
   signup = async (user) => {
-    return await UserServiceInstance.register(user);
+    const hashPassword = await this.hashPassword(user.password);
+    const data = {
+      ...user,
+      password: hashPassword,
+    };
+    return await UserServiceInstance.register(data);
+  };
+
+  hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
   };
 }
 
